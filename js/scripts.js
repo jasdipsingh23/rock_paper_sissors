425bb907d0ba8b97529a28826643c3f4317796ab
Jasdip Singh <jasdipsingh23@gmail.com>
2022-01-25 17:56:58 -0800

    Removing console logs

--- js/scripts.js
+++ js/scripts.js
@@ -9,26 +9,26 @@ function playRound(playerSelection, computerSelection) {
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
 
-    if(player == "rock") {
-        if(computer == "scissors") {
+    if (player == "rock") {
+        if (computer == "scissors") {
             return "You win! Rock beats Scissors!";
-        } else if(computer == "paper") {
+        } else if (computer == "paper") {
             return "You lose! Paper beats Rock.";
         } else {
             return "Tie game!";
         }
-    } else if(player == "scissors") {
-        if(computer == "scissors") {
+    } else if (player == "scissors") {
+        if (computer == "scissors") {
             return "Tie game!";
-        } else if(computer == "paper") {
+        } else if (computer == "paper") {
             return "You win! Scissors beats Paper.";
         } else {
             return "You lose! Rock beats Scissors.";
         }
     } else {
-        if(computer == "scissors") {
+        if (computer == "scissors") {
             return "You lose! Scissors beats Paper!";
-        } else if(computer == "paper") {
+        } else if (computer == "paper") {
             return "Tie game!";
         } else {
             return "You win! Paper beats Rock.";
@@ -50,7 +50,6 @@ btnList.forEach((button) => {
     button.addEventListener('click', () => {
         let playerSelection = button.id;
         let result = playRound(playerSelection, computerPlay());
-        console.log(result);
         document.getElementById("content").textContent = result;
     });
 });
